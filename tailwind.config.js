580117b19189d7b1d8ea914d9212eaaee2a2f4b9
abhi62 <abhi.leon.93@gmail.com>
2022-01-07 08:42:05 +0530

    - for test

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,30 +1,37 @@
 module.exports = {
-  content: ['./src/**/*.{js,jsx,ts,tsx}'],
-  theme: {
-    extend: {
-      colors: {
-        theme: '#2A2A2A',
+    purge: [],
+    darkMode: false, // or 'media' or 'class'
+    theme: {
+      extend: {
+        colors:{
+          theme : '#2A2A2A',
+          primary : '#5F9EA0' ,
+          
+        },
+        fontFamily : {
+          mont : ['Montserrat' , 'sans-serif']
+        }
       },
-      fontFamily: {
-        mont: ['Montserrat', 'sans-serif'],
-      },
+      screens: {
+        '2xl': {'max': '1835px'},
+        // => @media (max-width: 1535px) { ... }
+  
+        'xl': {'max': '1279px'},
+        // => @media (max-width: 1279px) { ... }
+  
+        'lg': {'max': '1023px'},
+        // => @media (max-width: 1023px) { ... }
+  
+        'md': {'max': '767px'},
+        // => @media (max-width: 767px) { ... }
+  
+        'sm': {'max': '639px'},
+        // => @media (max-width: 639px) { ... }
+      }
     },
-    screens: {
-      '2xl': { max: '1535px' },
-      // => @media (max-width: 1535px) { ... }
-
-      xl: { max: '1279px' },
-      // => @media (max-width: 1279px) { ... }
-
-      lg: { max: '1023px' },
-      // => @media (max-width: 1023px) { ... }
-
-      md: { max: '500px' },
-      // => @media (max-width: 767px) { ... }
-
-      sm: { max: '439px' },
-      // => @media (max-width: 639px) { ... }
+    variants: {
+      extend: {},
     },
-  },
-  plugins: [],
-};
+    
+    plugins: [],
+  }
